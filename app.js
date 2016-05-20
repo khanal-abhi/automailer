2db76bf9adba92f1f2d5fd0010c90186e8c816f9
Abhi Khanal <khanal.abhi@gmail.com>
2016-05-20 10:34:40 -0400

    have some basic api functionality with users persistence to mongodb.

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var MongoClient = require('mongodb').MongoClient;
+var db = null;
+
+MongoClient.connect("mongodb://localhost:27017/automailer", function(err, database){
+  if(!err){
+    console.log("Connection established.");
+    db = database;
+  }
+});
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -19,11 +29,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// attach the db to the req
+app.use(function(req, res, next){
+  req.db = db;
+  next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,104 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var db = req.db;
+  var addresses = db.collection('addresses');
+  addresses.find().toArray(function(err, items){
+    res.send(items);
+  });
+});
+
+/* POST a new user. */
+router.post('/', function(req, res, next){
+  var db = req.db;
+  var username = req.body.name;
+  var email = req.body.email;
+
+  if (username && email){
+    var user = {
+      name: username,
+      'email': email
+    };
+    var addresses = db.collection('addresses');
+    addresses.insert(user, function(err, result){
+      if(!err){
+        var formatted_message = {
+          summary: 'Email added succesfully.',
+          added_user: user
+        }
+        //TODO: send email confirmation the user
+        res.send(formatted_message);
+      }
+    });
+  } else {
+    var errors = [];
+    if(!username){
+      errors.push('No username');
+    }
+    if(!email){
+      errors.push('No email');
+    }
+    var result = {
+      summary: 'Could not add the email.',
+      'errors': errors
+    };
+    res.send(result);
+  }
+
+});
+
+/* Use nodemailer to send the email to all the users */
+router.post('/send-message', function(req, res, next){
+  var subject = req.body.subject;
+  var message = req.body.message;
+  var errors = [];
+
+  if(!subject){
+    errors.push('No subject');
+  }
+
+  if(!message){
+    errors.push('No message');
+  }
+
+  var db = req.db;
+  var users = db.collection('addresses');
+  users.find({}, function(err, items){
+    if(!err){
+      var result = [];
+      for(var i = 0; i < items.lenght; i++){
+        result.push({
+          email: items[i].email,
+          'subject': subject,
+          'message': message
+        });
+      }
+    } else {
+      res.send({
+        summary: 'emails could not be sent',
+        errors: []
+      });
+    }
+  });
+
+});
+
+/* GET all the emails in the list */
+router.get('/emails', function(req, res, next){
+  var db = req.db;
+  var users = db.collection('addresses');
+  users.find().toArray(function(err, items){
+    var emails = [];
+    for(var i = 0; i < items.length; i++){
+      var user = {
+        name: items[i].name,
+        email: items[i].email
+      }
+      emails.push(user);
+    }
+    res.send(emails);
+  });
+
 });
 
 module.exports = router;
